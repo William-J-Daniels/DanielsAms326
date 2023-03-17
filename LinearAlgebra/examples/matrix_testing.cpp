41e6d66be4f4213c03a3d614200fcda70ef9b744
William Daniels <William.J.Daniels@protonmail.com>
2023-03-17 15:36:03 -0400

    Added iterators for matrix slicing

--- LinearAlgebra/examples/matrix_testing.cpp
+++ LinearAlgebra/examples/matrix_testing.cpp
@@ -3,11 +3,32 @@
 
 int main()
 {
-    auto Test1 = la::Matrix<double>(std::pow(2, 10), std::pow(2, 10));
-    auto Test2 = la::Matrix<double>(std::pow(2, 10), std::pow(2, 10));
+    // auto Test1 = la::Matrix<double>(std::pow(2, 10), std::pow(2, 10));
+    // auto Test2 = la::Matrix<double>(std::pow(2, 10), std::pow(2, 10));
+    //
+    // Test1.rand(0.3, 0.305);
+    // Test2.rand(0.333, 0.305);
+    //
+    // auto Test3 = Test1.naive_mult(Test2);
 
-    Test1.rand(0.3, 0.305);
-    Test2.rand(0.333, 0.305);
+    auto Test = la::Matrix<double>(5,5);
+    Test.rand(0.5,0.5127);
 
-    auto Test3 = Test1.naive_mult(Test2);
+    std::cout << Test << std::endl;
+    std::cout << Test(1,1) << " -> " << Test(3,3) << std::endl;
+    std::cout << "begin loop" << std::endl;
+    for (auto p = Test.slice_begin(1,1,2,2);  p != Test.slice_end(1,1,2,2); p++)
+    {
+        std::cout << *p << " ";// << std::endl;
+    }
+
+    for (auto p = Test.row_begin(); p != Test.row_end(); p++)
+    {
+        std::cout << *p << " ";
+    }
+    std::cout << std::endl;
+    for (auto p = Test.column_begin(); p != Test.column_end(); p++)
+    {
+        std::cout << *p << " ";
+    }
 }
--- LinearAlgebra/include/matrix.h
+++ LinearAlgebra/include/matrix.h
@@ -63,16 +63,20 @@ public:
         columnIterator& operator++()
         {
             if (counter%M.columns != 0)
-            {
+            { // when not at the end
+                // stride by one row
                 m_ptr = m_ptr + M.rows;
                 counter = counter + 1;
                 return *this;
             }
             if (counter == M.rows*M.columns)
-            {
+            { // if we've visited all the addresses
+                // stride by one address
                 m_ptr = m_ptr + 1;
                 return *this;
             }
+            // when at the end of a column
+            // stride to the next column
             m_ptr = m_ptr - M.rows * (M.columns - 1) + 1;
             counter = counter + 1;
             return *this;
@@ -90,6 +94,63 @@ public:
     };
     auto column_begin() {return columnIterator(&data[0],            *this);}
     auto column_end()   {return columnIterator(&data[rows*columns], *this);}
+    class Slice
+    {
+        using iterator_category = std::forward_iterator_tag;
+        using difference_type   = std::ptrdiff_t;
+        using value_type        = T;
+        using pointer           = value_type*;
+        using reference         = value_type&;
+    public:
+        Slice(pointer ptr, Matrix &Owner,
+              std::size_t start_row, std::size_t start_col,
+              std::size_t end_row, std::size_t end_col) :
+              m_ptr(ptr), M(Owner),
+              sr(start_row), sc(start_col),
+              er(end_row), ec(end_col) {}
+        reference operator*() const {return *m_ptr;}
+        pointer operator->() {return m_ptr;}
+        Slice& operator++()
+        {
+            if (counter%((ec-sc)+1) == 0)
+            { // when we're not the end of the slice's row
+                std::cout <<"end of row, " << counter << std::endl;
+                // get to the next row of the slice
+                m_ptr += M.columns-(ec-sc);
+                counter += 1;
+                return *this;
+            }
+            // default behavior is to advance by one
+            std::cout << "default" << ", " << counter << std::endl;
+            m_ptr += 1;
+            counter += 1;
+            return *this;
+        }
+        Slice operator++(int)
+            {Slice tmp = *this; ++(*this); return tmp;}
+        friend bool operator== (const Slice& a,const Slice& b)
+            {return a.m_ptr == b.m_ptr;}
+        friend bool operator!= (const Slice& a,const Slice& b)
+            {return a.m_ptr != b.m_ptr;}
+    private:
+        pointer m_ptr;
+        Matrix<T> M;
+        size_t sr, sc, er, ec;
+        unsigned counter = 1;
+    };
+    auto slice_begin(std::size_t start_row, std::size_t start_col,
+                     std::size_t end_row,   std::size_t end_col)
+    {
+        return Slice(&data[start_row*rows+start_col], *this,
+                     start_row, start_col, end_row, end_col);
+    }
+    auto slice_end(std::size_t start_row, std::size_t start_col,
+                   std::size_t end_row,   std::size_t end_col)
+    {
+        return Slice(
+            &data[(end_row+1)*rows+(end_col-(end_col-start_col))], *this,
+            start_row, start_col, end_row, end_col);
+    }
 
     // matrix operations (will grow over time)
     Matrix<T> operator- ();
@@ -281,7 +342,7 @@ void Matrix<T>::mult_helper(Matrix<T>& M2, Matrix<T>& Mout, size_t thread_idx)
             // add the index to the start, because this is equal to the number
             // of threads before it that had to do an extra loop,
             start += thread_idx;
-            // and add teh index to the end, becuase this thread doesn't need to
+            // and add the index to the end, becuase this thread doesn't need to
             // do extra work.
             end += thread_idx;
         }
@@ -313,7 +374,7 @@ Matrix<T> Matrix<T>::strassen_mult(Matrix<T> M2)
     /*
      * An obvious way to thread Strassen's method is to create a thread for each
      * of the 7 child multiplications. On my system, this is unideal becuase I
-     * only have two physical cores and four loical threads. The result of this
+     * only have two physical cores and four logcal threads. The result of this
      * would be multiple naive multiplications running in parallel, but this is
      * no more efficient than performing each multiplication with a threaded
      * implimentation serially. So, I only have to impliment Strassen in a
@@ -321,8 +382,23 @@ Matrix<T> Matrix<T>::strassen_mult(Matrix<T> M2)
      * written naive algorithm.
      */
     assert(rows == columns && rows%2 == 0); // will impliment padding later
-}
+    auto newMatrix = Matrix<T>(rows, M2.columns);
 
+    std::size_t size = rows/2;
+    std::vector<Matrix<T>> Intermediates(8, Matrix(size, size));
+
+    for (size_t i = 0; i < size; i++)
+    {
+        for (unsigned j = 0; j < size; j++)
+        {
+            Intermediates[0].set(i,j, (i,j));
+            Intermediates[1].set(i,j, (i,size+j));
+            Intermediates[2].set(i,j, (size+i,j));
+            Intermediates[3].set(i,j, (size+i,size+j));
+
+        }
+    }
+}
 
 } // namespace la
 
