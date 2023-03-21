deaeb0a0a8ced63cbc16ce266d7b5a96a1d61751
William Daniels <William.J.Daniels@protonmail.com>
2023-03-21 15:06:26 -0400

    Gausian elimination

--- LinearAlgebra/examples/solver_testing.cpp
+++ LinearAlgebra/examples/solver_testing.cpp
@@ -7,16 +7,21 @@ int main()
                           {-1, 2,  0},
                           {2,  0,  1}}};
     std::vector<double> b{6, 3, 5};
+    // la::Matrix<double> A{{{{1,  2,  3,  4},
+    //                        {5,  1,  6, -1},
+    //                        {10, 2, 13, -2},
+    //                        {4, 10, -2, -5}}}};
+    // std::vector<double> b{3,  2,  4,  1};
     // auto A = la::Matrix<double>(10, 10);
     // A.rand(0.01, 0.01);
     // std::vector<double> b(10);
 
     auto Sln = la::LinearSolver(A, b);
 
-    std::cout << A << std::endl;
-    Sln.gaus_elim();
+    auto Results = Sln.gaus_elim();
 
-    return 0;
+    for (auto& R : Results)
+        std::cout << R << " ";
 
     return 0;
 }
--- LinearAlgebra/include/linearsolver.h
+++ LinearAlgebra/include/linearsolver.h
@@ -7,6 +7,7 @@
 #include <thread>
 #include <algorithm>
 #include <cmath>
+#include <limits>
 
 #include <iostream>
 
@@ -19,23 +20,28 @@ class LinearSolver
     // takes a reference to the data, if you need the original make a copy
 public:
     LinearSolver() = default;
-    LinearSolver(Matrix<T>& init_A, std::vector<T> init_b);
+    LinearSolver<T>(Matrix<T> init_A, std::vector<T> init_b);
 
     std::vector<T> gaus_elim();
 
 private:
     Matrix<T> A;
     std::vector<T> b;
+    std::vector<T> Solutions;
 
     unsigned num_pivots = 0;
 
     std::vector<T> find_scales();
     void find_scales_helper(unsigned start, unsigned end,
                             std::vector<T>& Scales);
+    void swap_rows(std::size_t north, std::size_t south);
+    void forward_eliminate(std::size_t startr, std::size_t endr,
+                           std::size_t current_row);
+    void back_substitution();
 };
 
 template <class T>
-LinearSolver<T>::LinearSolver(Matrix<T>& init_A, std::vector<T> init_b)
+LinearSolver<T>::LinearSolver(Matrix<T> init_A, std::vector<T> init_b)
 {
     assert(init_A.numrows() == init_b.size());
     assert(init_A.numrows() == init_A.numcolumns()); // only square for now
@@ -47,10 +53,78 @@ LinearSolver<T>::LinearSolver(Matrix<T>& init_A, std::vector<T> init_b)
 template <class T>
 std::vector<T> LinearSolver<T>::gaus_elim()
 {
-    std::vector<T> Solutions(A.numcolumns());
-    std::vector<T> TempRow(A.numcolumns());
+    Solutions = std::vector<T>(A.numcolumns());
+    auto Scales = find_scales();
+    double scaled_col, col_max, row_max_idx;
+    std::size_t mod, start, end; // for distributing work to threads
 
-    find_scales();
+    for (std::size_t current_row = 0; current_row < A.numrows(); current_row++)
+    { // loop over all rows
+        std::vector<std::thread> Threads;
+        // first we loop for pivoting opperturnities
+        col_max = std::numeric_limits<T>::lowest();
+        row_max_idx = current_row;
+        for (std::size_t prow = current_row; prow < A.numrows(); prow++)
+        {
+            scaled_col = std::abs(A(prow, current_row)) / Scales[prow];
+            if (scaled_col > col_max)
+            {
+                col_max = scaled_col;
+                row_max_idx = prow;
+            }
+        }
+        if (row_max_idx-current_row)
+            swap_rows(row_max_idx, current_row);
+
+        // then we forward eliminate
+
+        /* parallelize later */
+        for (std::size_t erow = current_row + 1; erow < A.numrows(); ++erow)
+        {
+            auto factor = A(erow, current_row) / A(current_row, current_row);
+            for (std::size_t ecol = current_row + 1; ecol < A.numcolumns(); ++ecol)
+            {
+                A.set(erow, ecol, A(erow,ecol) - A(current_row, ecol) * factor);
+            }
+            A.set(erow, current_row, 0.0);
+            b[erow] = b[erow] - b[current_row]*factor;
+        }
+
+        // this bit of code apears multiple times across this class and matrix,
+        // it should be moved to a function
+        // for (std::size_t t = 0; t < std::thread::hardware_concurrency(); t++)
+        // {
+        //     // load balance is less than ideal, but better approaches are too
+        //     // sophisticated for me
+        //     // https://cs.wmich.edu/elise/courses/cs626/s12/cs6260_presentation_1.pdf
+        //     mod = (A.numrows()-current_row)%std::thread::hardware_concurrency();
+        //     start = (A.numrows()-current_row)/std::thread::hardware_concurrency()*t;
+        //     end = (A.numrows()-current_row)/std::thread::hardware_concurrency()*(t+1);
+        //     if(mod)
+        //     {
+        //         if (t < mod)
+        //         {
+        //             start += t;
+        //             end   += t+1;
+        //         } else {
+        //             start += mod;
+        //             end   += mod;
+        //         }
+        //     }
+        //     if (start != end)
+        //     {
+        //         std::cout << "make thread on row" << current_row << std::endl;
+        //         Threads.push_back(std::thread(
+        //             &LinearSolver::forward_eliminate, this, start, end,
+        //             current_row
+        //         ));
+        //     }
+        // }
+        // for (auto & tr : Threads)
+        //     tr.join();
+
+    }
+    back_substitution();
 
     return Solutions;
 }
@@ -93,9 +167,6 @@ std::vector<T> LinearSolver<T>::find_scales()
     for (auto& t : Threads)
         t.join();
 
-    for (auto& s : Scales)
-        std::cout << s << ", ";
-
     return Scales;
 }
 
@@ -110,6 +181,76 @@ void LinearSolver<T>::find_scales_helper(unsigned start, unsigned end,
             A.slice_end(i,0,i,A.numcolumns()-1),
             [] (T a, T b) { return std::abs(a) < std::abs(b); }
         );
+    }
+}
+
+template <class T>
+void LinearSolver<T>::swap_rows(std::size_t north, std::size_t south)
+{
+    // can be parallelized later
+    std::vector<T> RowTemp(A.numcolumns());
+    T VecTemp;
+
+    std::copy(
+        A.slice_begin(north,0,north,A.numcolumns()),
+        A.slice_end(north,0,north,A.numcolumns()),
+        RowTemp.begin()
+    );
+
+    std::copy(
+        A.slice_begin(south,0,south,A.numcolumns()),
+        A.slice_end(south,0,south,A.numcolumns()),
+        A.slice_begin(north,0,north,A.numcolumns())
+    );
+
+    std::copy(
+        RowTemp.begin(),
+        RowTemp.end(),
+        A.slice_begin(south,0,south,A.numcolumns())
+    );
+
+    VecTemp = b[north];
+    b[north] = b[south];
+    b[south] = VecTemp;
+
+    num_pivots++;
+}
+
+template <class T>
+void LinearSolver<T>::forward_eliminate(std::size_t startr, std::size_t endr,
+                                        std::size_t current_row)
+{
+    T factor;
+    for (std::size_t i = startr; i < endr; i++)
+    {
+        factor = A(i, current_row) / A(current_row, current_row);
+        //std::cout << " " << startr << " " << factor << std::endl;
+        // std::transform(
+        //     A.slice_begin(i, i, i, A.numcolumns()-1),
+        //     A.slice_end(i, i, i, A.numcolumns()-1),
+        //     A.slice_begin(i, i, i, A.numcolumns()-1),
+        //     [&] (T x) { return x - 5; }
+        // );
+        /* almost there, just need to get the right factor and switch the
+         * transform to subtract. need to use two slices on transform
+         */
+    }
+}
+
+template <class T>
+void LinearSolver<T>::back_substitution()
+{
+    Solutions[A.numcolumns()-1] = b[A.numcolumns()-1]/A(A.numcolumns()-1, A.numcolumns()-1);
+
+    for (int brow = (A.numcolumns()-2); brow >= 0; --brow)
+    {
+        //std::cout << brow << std::endl;
+        double sum = b[brow];
+        for (std::size_t bcol = brow+1; bcol < A.numcolumns(); ++bcol)
+        {
+            sum = sum - A(brow, bcol) * Solutions[bcol];
+        }
+        Solutions[brow] = sum / A(brow, brow);
     }
 }
 
