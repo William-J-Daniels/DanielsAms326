40b4a6835fe52cca06ff67d0e9bd33b5b551bcbd
William Daniels <William.J.Daniels@protonmail.com>
2023-04-12 14:00:39 -0400

    Hw3P1 with gauss

---
+++ HomeworkThree/examples/problem_one.cpp
@@ -0,0 +1,27 @@
+#include <matrix.h>
+#include <linearsolver.h>
+
+#include <iostream>
+
+
+int main()
+{
+    la::Matrix<double> A{{{4, -1,  0, -1,  0,  0,  0,  0,  0},
+                          {-1, 4, -1,  0, -1,  0,  0,  0,  0},
+                          {0, -1,  4,  0,  0, -1,  0,  0,  0},
+                          {-1, 0,  0,  4, -1,  0, -1,  0,  0},
+                          {0, -1,  0, -1,  4, -1,  0, -1,  0},
+                          {0,  0, -1,  0, -1,  4,  0,  0, -1},
+                          {0,  0,  0, -1,  0,  0,  4, -1,  0},
+                          {0,  0,  0,  0, -1,  0, -1,  4, -1},
+                          {0,  0,  0,  0,  0, -1,  0, -1,  4}}};
+    std::vector<double> b = {1, 1, 1, 1, 1, 1, 1, 1, 1};
+
+    auto Solver = la::LinearSolver(A, b);
+    auto result = Solver.gaus_elim();
+    std::cout << "Gaussian elimination solution:" << std::endl;
+    for (auto& r : result)
+        std::cout << r << " ";
+    std::cout << std::endl;
+    return 0;
+}
--- LinearAlgebra/examples/solver_testing.cpp
+++ LinearAlgebra/examples/solver_testing.cpp
@@ -3,15 +3,15 @@
 
 int main()
 {
-    la::Matrix<double> A{{{1,  1,  1},
-                          {-1, 2,  0},
-                          {2,  0,  1}}};
-    std::vector<double> b{6, 3, 5};
-    // la::Matrix<double> A{{{{1,  2,  3,  4},
-    //                        {5,  1,  6, -1},
-    //                        {10, 2, 13, -2},
-    //                        {4, 10, -2, -5}}}};
-    // std::vector<double> b{3,  2,  4,  1};
+    // la::Matrix<double> A{{{1,  1,  1},
+    //                       {-1, 2,  0},
+    //                       {2,  0,  1}}};
+    // std::vector<double> b{6, 3, 5};
+    la::Matrix<double> A{{{{1,  2,  3,  4},
+                           {5,  1,  6, -1},
+                           {10, 2, 13, -2},
+                           {4, 10, -2, -5}}}};
+    std::vector<double> b{3,  2,  4,  1};
     // auto A = la::Matrix<double>(10, 10);
     // A.rand(0.01, 0.01);
     // std::vector<double> b(10);
