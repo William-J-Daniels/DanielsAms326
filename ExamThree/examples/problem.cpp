ba56e2b4b37edf17d968dfcc7bde410625b323f7
William Daniels <William.J.Daniels@protonmail.com>
2023-04-18 18:03:30 -0400

    Exam 3 commit

---
+++ ExamThree/examples/problem.cpp
@@ -0,0 +1,94 @@
+// my libraries
+#include <euler.h>
+
+// cpp standard libraries
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include <execution>
+#include <list>
+#include <array>
+#include <string>
+#include <fstream>
+
+// constants
+const unsigned RES = 1024; // resolution of search grid
+const double MIN_ANG = -1.4; // angle search space
+const double MAX_ANG =  1.4;
+const double STEP = 1.0e-4; // set size in miles
+// easier to read than magic numbers
+const double a = 8.0;
+const double v0 = 20.0, vb = 30.0;
+
+int main()
+{
+    // make search space
+    std::vector<double> betas (RES);
+    std::vector<double> results (RES);
+
+    // populate search vector
+    #pragma omp parallel for
+    for (unsigned i = 0; i < RES; i++)
+        betas[i] = i * (RES+1) * (MAX_ANG - MIN_ANG) / (RES*RES) + MIN_ANG;
+
+    // brute force search for the optimal path
+    #pragma omp parallel for
+    for (unsigned i = 0; i < RES; i++)
+    {
+        double d_travel = 0.0;
+        auto DE = [=] (double x) {
+            return (v0 / vb) * (1.0 - ((x*x)/(a*a))) / std::cos(betas[i]) +
+                   std::tan(betas[i]);
+        };
+        auto Int = ode::Euler(DE, -a, 0, STEP);
+        while (Int.get_x() < a)
+        {
+            auto prev_y = Int.get_y();
+            Int.advance(STEP);
+            d_travel += std::sqrt(
+                STEP*STEP + (prev_y-Int.get_y())*(prev_y-Int.get_y())
+            );
+        }
+        results[i] = d_travel;
+    }
+
+    // find the minimum travel and angle
+    auto min = std::min_element(
+        std::execution::par, results.begin(), results.end()
+    );
+    auto min_idx = std::distance(min, results.begin());
+
+    // create list for a plot
+    std::list<std::array<double, 2>> plot;
+
+    auto DE = [=] (double x) {
+        return (v0 / vb) * (1.0 - ((x*x)/(a*a))) / std::cos(betas[min_idx]) +
+               std::tan(betas[min_idx]);
+    };
+    auto Int = ode::Euler(DE, -a, 0, STEP);
+    while (Int.get_x() < a)
+    {
+        Int.advance(STEP);
+        plot.push_back({Int.get_x(), Int.get_y()});
+    }
+
+    // write csv files to be used with python for plots
+    std::string OptFilename = "../../ExamThree/data/travel.csv";
+    std::ofstream OptFile(OptFilename);
+    OptFile << "beta,d_travel" << std::endl;
+    for (unsigned i = 0; i < RES; i++)
+        OptFile << betas[i] << "," << results[i] << std::endl;
+    OptFile.close();
+
+    std::string PathFilename = "../../ExamThree/data/shortest.csv";
+    std::ofstream PathFile(PathFilename);
+    PathFile << "x,y" << std::endl;
+    for (auto& c : plot)
+        PathFile << c[0] << "," << c[1] << std::endl;
+    PathFile.close();
+
+    std::cout << "The optimal angle is " << betas[min_idx] << ", producing a"
+              << " distance traved of " << *min << std::endl;
+
+    return 0;
+}
---
+++ ExamThree/tools/plots.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+Opt = pd.read_csv("../data/travel.csv")
+Short = pd.read_csv("../data/shortest.csv")
+
+figa, axa = plt.subplots()
+axa.set_title("Angle vs path length")
+axa.set_xlabel("Angle")
+axa.set_ylabel("Path length")
+axa.plot(Opt['beta'], Opt['d_travel'])
+figa.savefig("../data/opt_curve.jpeg")
+
+figb, axb = plt.subplots()
+axb.set_title("Shortest path")
+axb.plot(Short['x'], Short['y'])
+figb.savefig("../data/shortest_path.jpeg")
